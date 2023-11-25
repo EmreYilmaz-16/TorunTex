f0d7f4a82210b90bb2676421d940228b5f34fac0
Emre YILMAZ <emre.yilmaz@hotmail.co.uk>
2023-11-25 13:45:05 +0300

    operat,r

--- js/Operator.js
+++ js/Operator.js
@@ -71,21 +71,25 @@ function getOrders(product_id) {
       success: function (retDat) {
         console.log(retDat);
         var arr = JSON.parse(retDat);
-        console.log(arr);
-        var control = $select[0].selectize;
-        control.clear();
-        control.clearOptions();
-        for (let i = 0; i < arr.length; i++) {
-          /* var opt=document.createElement("option");
+        if (arr.length != 0) {
+          console.log(arr);
+          var control = $select[0].selectize;
+          control.clear();
+          control.clearOptions();
+          for (let i = 0; i < arr.length; i++) {
+            /* var opt=document.createElement("option");
             opt.setAttribute("value",arr[i].ORDER_ROW_ID);
             opt.innerText=arr[i].ORDER_NUMBER;
             document.getElementById("select_2").appendChild(opt);*/
-          control.addOption({
-            ORDER_ROW_ID: arr[i].ORDER_ROW_ID,
-            ORDER_NUMBER: arr[i].ORDER_NUMBER,
-            NICKNAME: arr[i].NICKNAME,
-            ORDER_HEAD: arr[i].ORDER_HEAD,
-          });
+            control.addOption({
+              ORDER_ROW_ID: arr[i].ORDER_ROW_ID,
+              ORDER_NUMBER: arr[i].ORDER_NUMBER,
+              NICKNAME: arr[i].NICKNAME,
+              ORDER_HEAD: arr[i].ORDER_HEAD,
+            });
+          }
+        }else {
+          TemizleCanim();
         }
       },
     });
