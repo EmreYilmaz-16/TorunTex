9912d4bb162d88674b003fe4f4e37d7ff318d30f
Emre YILMAZ <emre.yilmaz@hotmail.co.uk>
2023-12-19 13:13:38 +0300

    taşıma yeni versiyon

--- js/tasima.js
+++ js/tasima.js
@@ -10,9 +10,13 @@ var WRK_ROW_ID_IN = "";
 
 var LOT_NUMARA = "";
 var STOCK_ID = "";
+var ISLEM = "";
+//BILGI ISLEM : 1 - SEÇİLİ DEPONUN SİPARİŞNDE VAR SEÇİLİ DEPO SİPARİŞİNDEN  WRK_ROW ID ALINACAK
+//BILGI ISLEM : 2 - SEÇİLİ DEPO SİPARİŞİNDE YOK  WRK_ROW_ID TEMIZLENECEK
+var AktifSiparisSureci = 259;
 $(document).ready(function () {
   $("#DEPARTMENT_OUT_SEL").html(
-    "<option value=''>Giriş Deposu Seçiniz</option>"
+    "<option value=''>Giriş Deposu Seçiniz</option><option value='15-2'>KLB</option><option value='15-3'>SCK</option><option value='15-1'>GRB</option>"
   );
 });
 
@@ -35,6 +39,7 @@ function getBarkode_1(el, ev) {
       LOCATION_OUT = QueryResult_1.LOCATION_ID[0];
       DEPARTMENT_OUT = QueryResult_1.DELIVER_DEPT_ID[0];
       WRK_ROW_ID_OUT = QueryResult_1.WRK_ROW_ID[0];
+      STOCK_ID = QueryResult_1.STOCK_ID[0];
       getOutLocations();
     } else {
       alert("Ürün Bulunamadı");
@@ -43,12 +48,11 @@ function getBarkode_1(el, ev) {
 }
 
 function getOutLocations() {
-
   $("#DEPARTMENT_OUT_SEL").html(
-    "<option value=''>Giriş Deposu Seçiniz</option>"
+    "<option value=''>Giriş Deposu Seçiniz</option><option value='15-2'>KLB</option><option value='15-3'>SCK</option><option value='15-1'>GRB</option>"
   );
   $("#DEPARTMENT_OUT_SEL").focus();
-  
+  document.getElementById(DEPARTMENT_OUT_SEL).removeAttribute("style");
   var Qstr1 =
     "SELECT D.DEPARTMENT_HEAD,SL.COMMENT,D.DEPARTMENT_ID,SL.LOCATION_ID FROM STOCKS_LOCATION AS SL ";
   Qstr1 +=
@@ -65,6 +69,51 @@ function getOutLocations() {
     document.getElementById("DEPARTMENT_OUT_SEL").appendChild(option);
   }
   $("#DEPARTMENT_OUT_SEL").click();
+  document
+    .getElementById("DEPARTMENT_OUT_SEL")
+    .setAttribute("style", "box-shadow: 1px 1px 20px 1px green;");
+}
+function setDO(el) {
+  var DEPARTMENT_ID_ = parseInt(list_getat(el.value, 1, "-"));
+  var LOCATION_ID_ = parseInt(list_getat(el.value, 2, "-"));
+  var Qstr1 =
+    "SELECT D.DEPARTMENT_HEAD,SL.COMMENT,D.DEPARTMENT_ID,SL.LOCATION_ID FROM STOCKS_LOCATION AS SL ";
+  Qstr1 +=
+    " INNER JOIN DEPARTMENT AS D ON D.DEPARTMENT_ID=SL.DEPARTMENT_ID     WHERE D.DEPARTMENT_ID=" +
+    DEPARTMENT_ID_ +
+    " AND SL.LOCATION_ID=" +
+    LOCATION_ID_;
+  var QueryResult_1 = (Qstr1, "dsn");
+
+  if (QueryResult_1.recordcount && DEPARTMENT_ID_ != 15) {
+    var Qstr2 =
+      "SELECT O.ORDER_ID,ORDER_NUMBER,ORDER_HEAD,ORR.WRK_ROW_ID,ORR.STOCK_ID FROM " +
+      dsn3 +
+      ".ORDERS AS O";
+    Qstr2 +=
+      " INNER JOIN " + dsn3 + ".ORDER_ROW AS ORR ON ORR.ORDER_ID=O.ORDER_ID ";
+    Qstr2 +=
+      " WHERE O.ORDER_STAGE=" +
+      AktifSiparisSureci +
+      " AND O.DELIVER_DEPT_ID=" +
+      QueryResult_1.DEPARTMENT_ID[0] +
+      " AND O.LOCATION_ID=" +
+      QueryResult_1.LOCATION_ID[0] +
+      "  AND ORR.STOCK_ID=" +
+      STOCK_ID;
+    var QueryResult_2 = wrk_query(Qstr2);
+
+    LOCATION_IN = LOCATION_ID_;
+    DEPARTMENT_IN = DEPARTMENT_ID_;
+    IN_TXT = QueryResult_1.DEPARTMENT_HEAD[0] + "-" + QueryResult_1.COMMENT[0];
+    WRK_ROW_ID_IN = QueryResult_2.WRK_ROW_ID;
+    document.getElementById(DEPARTMENT_OUT_SEL).removeAttribute("style");
+  } else {
+    LOCATION_IN = LOCATION_ID_;
+    DEPARTMENT_IN = DEPARTMENT_ID_;
+    IN_TXT = QueryResult_1.DEPARTMENT_HEAD[0] + "-" + QueryResult_1.COMMENT[0];
+    WRK_ROW_ID_IN = "";
+  }
 }
 
 function wrk_query(str_query, data_source, maxrows) {
